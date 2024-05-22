9908a70ce9cd713cd36548ab99c6d7bef295efe4
bobberg <bobberg90@hotmail.com>
2024-05-22 12:24:36 +0200

    fix uneeq error

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,16 @@
-import React, { useEffect } from "react";
+import React from "react";
 import "./App.css";
-import BelastingDienstLogo from "./assets/BelastingdienstLogo";
-
-declare global {
-  interface Window {
-    uneeqInteractionsOptions: any;
-  }
-}
+import BelastingDienstLogo from "./assets/BelastingdienstLogo"; // Ensure the import path and naming is correct
 
 function App() {
-  useEffect(() => {
-    window.uneeqInteractionsOptions = {
-      personaShareId: process.env.REACT_APP_PERSONA_SHARE_ID,
-      layoutMode: "fullScreen",
-      showUserInputInterface: true,
-      displayCallToAction: true,
-      enableMicrophone: true,
-    };
-
-    const script = document.createElement("script");
-    script.src = "https://hosted.eu.uneeq.io/interactions/v1/deploy";
-    script.async = true;
-
-    document.body.appendChild(script);
-
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
-
   return (
     <div className="App">
       <nav className="App-nav">
         <BelastingDienstLogo />
       </nav>
-      <header className="App-header"></header>
+      <header className="App-header">
+        <div id="digitalhuman"></div>
+      </header>
     </div>
   );
 }
